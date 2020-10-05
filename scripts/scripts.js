8a2e0db8d7b6c4cd7ffd0cbcc7e06c2cc1c597c5
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-10-04 23:12:55 -0400

    modified console log, comments and logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+// ASSIGNMENT 03 - Bryan Hughes - Student #N00481777
+
 //BOOK #1
 var bookOne = "Holy Bible - King James Version";
 //BOOK #2
@@ -31,27 +33,27 @@ var bookArray = [
     bookNine,
     bookTen
 ];
-//INITIALIZING 'gandalf' TO NULL
+//initializing a gate-keeper variable 'gandalf' to null
 var gandalf = null;
-//While 'gandalf' is equal to null, YOU SHALL NOT PASS!
+//while 'gandalf' has no value, YOU SHALL NOT PASS!
 while (!gandalf) {
-    //Prompt the user to enter a number between 1 and 10
-    //Parse the user's input to an int
+    //Prompt user to input number between 1 and 10 / Parse inputted value to an int
     var userInput = parseInt(prompt("Which top 10 book would you like?", "Pick a number: 1-10"));
     //IF userInput is between 1 and 10
     if (userInput >= 1 && userInput <= 10) {
         //Loop through the array of books
         bookArray.forEach(book => {
-            //IF (userInput - 1) is equal to the index of 'book' in 'bookArray'
+            //IF the userInput minus 1 is equal to the index of 'book' in 'bookArray'
             if (userInput - 1 === bookArray.indexOf(book)) {
-                //create a variable equal to the book found
+                //create a variable equal to the book found in the bookArray
                 var position = book;
                 //tell the user the book they selected
                 alert(`The number ${userInput} most influential book to humanity is "${position}"`);
-                console.log(`Book${userInput} - "${position}"`);
             }
+            //Log the index plus 1 of each book, plus the title of each book to the console
+            console.log(`Book ${bookArray.indexOf(book) + 1} - ${book}`);
         });
-        //set gandalf to userInput, you shall now pass
+        //set gandalf's value to userInput's value, YOU SHALL NOW PASS!
         gandalf = userInput;
     //ELSE IF userInput isn't a number, or less than 1 or greater than 10
     } else if (isNaN(userInput) || userInput < 1 || userInput > 10 || userInput === "Pick a number: 1-10") {
@@ -59,3 +61,6 @@ while (!gandalf) {
         alert("Please enter a number between 1 and 10!");
     }
 }
+
+/*NOTE: I had a lot of fun writing this assignment. It was challenging, but I think what
+I wrote is a good answer. Very interested in ways I could have written this better.*/
