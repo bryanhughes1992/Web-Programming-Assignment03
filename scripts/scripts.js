0c8bcbec3b80d29d715255b6d5f96e8d55442873
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-10-04 21:28:53 -0400

    final changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,23 @@
 //BOOK #1
-var bookOne = "#1 Holy Bible - King James Version";
+var bookOne = "Holy Bible - King James Version";
 //BOOK #2
-var bookTwo = "#2 The Republic - Plato";
+var bookTwo = "The Republic - Plato";
 //BOOK #3
-var bookThree = "#3 Being and Time - Martin Heidegger";
+var bookThree = "Being and Time - Martin Heidegger";
 //BOOK #4
-var bookFour = "#4 Das Kapital - Karl Marx";
+var bookFour = "Das Kapital - Karl Marx";
 //BOOK #5
-var bookFive = "#5 القرآن الكريم - Anonymous";
+var bookFive = "القرآن الكريم - Anonymous";
 //BOOK #6
-var bookSix = "#6 Critique of Pure Reason - Immanuel Kant";
+var bookSix = "Critique of Pure Reason - Immanuel Kant";
 //BOOK #7
-var bookSeven = "#7 The Origin of Species - Charles Darwin";
+var bookSeven = "The Origin of Species - Charles Darwin";
 //BOOK #8
-var bookEight = "#8 1984 - George Orwell";
+var bookEight = "1984 - George Orwell";
 //BOOK #9
-var bookNine = "#9 Steppenwolf - Hermann Hesse";
+var bookNine = "Steppenwolf - Hermann Hesse";
 //BOOK #10
-var bookTen = "#10 Faust, First Part - Johann Wolfgang von Goethe";
+var bookTen = "Faust, First Part - Johann Wolfgang von Goethe";
 //Array of Top Ten Most Influential Books to Mankind
 var bookArray = [
     bookOne,
@@ -42,18 +42,19 @@ while (!gandalf) {
     if (userInput >= 1 && userInput <= 10) {
         //Loop through the array of books
         bookArray.forEach(book => {
-            //IF userInput - 1 is equal to the index of 'book' in 'bookArray'
+            //IF (userInput - 1) is equal to the index of 'book' in 'bookArray'
             if (userInput - 1 === bookArray.indexOf(book)) {
                 //create a variable equal to the book found
                 var position = book;
                 //tell the user the book they selected
-                alert(`Number ${userInput} on the list is ${position}`);
+                alert(`The number ${userInput} most influential book to humanity is ${position}`);
             }
         });
-        //set gandalf to userInput, you shall pass
+        //set gandalf to userInput, you shall now pass
         gandalf = userInput;
     //ELSE IF userInput isn't a number, or less than 1 or greater than 10
     } else if (isNaN(userInput) || userInput < 1 || userInput > 10 || userInput === "Pick a number: 1-10") {
+        //Tell user to re-enter a proper number
         alert("Please enter a number between 1 and 10!");
     }
 }
